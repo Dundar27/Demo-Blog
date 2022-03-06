5102ba617f8d2f740a9ffb3d9a1a90973d16d453
Dundar27 <davutburakduundar@gmail.com>
2022-03-06 12:33:11 +0300

    commit text

--- src/components/header.js
+++ src/components/header.js
@@ -10,58 +10,59 @@ function Header() {
         onSnapshot(collection(db, "post"), snapshop => setvalues(snapshop.docs.map(doc => ({
             id:doc.id,data:doc.data()
         }))))
-    },[])
-        
-    console.log(values[0])
+    },[])      
+    console.log(values[0].map(value => (value.data.title)))
+
     const database = {
         slide1: {
-            id: values[0].data.id,
-            src: values[0].data.img_src,
-            title: values[0].data.title,
-            description: values[0].data.description
+            id: values[0].map(value => (value.data.id)),
+            src: values[0].map(value => (value.data.img_src)),
+            title: values[0].map(value => (value.data.title)),
+            description: values[0].map(value => (value.data.description))
         },
         slide2: {
-            id: values[1].data.id,
-            src: values[1].data.img_src,
-            title: values[1].data.title,
-            description: values[1].data.description
+            id: values[1].map(value => (value.data.id)),
+            src: values[1].map(value => (value.data.img_src)),
+            title: values[1].map(value => (value.data.title)),
+            description: values[1].map(value => (value.data.description))
         },
         slide3: {
-            id: values[2].data.id,
-            src: values[2].data.img_src,
-            title: values[2].data.title,
-            description: values[2].data.description
+            id: values[2].map(value => (value.data.id)),
+            src: values[2].map(value => (value.data.img_src)),
+            title: values[2].map(value => (value.data.title)),
+            description: values[2].map(value => (value.data.description))
         }
-        
     }
-    
+
     return (
         
         <div className='p-5'>
             <div id="carouselBasicExample" className="carousel slide carousel-fade" data-mdb-ride="carousel">
+                {
                 <div className="carousel-inner"> 
                     <div className="carousel-item" id={database.slide1.id} style={{opacity: "100", display: "block"}}>
-                       <img src={database.slide1.src} className="d-block w-100" alt="Canyon at Nigh"/>
-                       <div className="carousel-caption d-none d-md-block">
-                           <h5>{database.slide1.title}</h5>
-                           <p>{database.slide1.description}</p>
-                       </div>  
-                   </div>
-                   <div className="carousel-item" id={database.slide2.id} style={{opacity: "100", display: "block"}}>
-                       <img src={database.slide2.src} className="d-block w-100" alt="Canyon at Nigh"/>
-                       <div className="carousel-caption d-none d-md-block">
-                           <h5>{database.slide2.title}</h5>
-                           <p>{database.slide2.description}</p>
-                       </div>  
-                   </div>
-                   <div className="carousel-item" id={database.slide3.id} style={{opacity: "100", display: "block"}}>
-                       <img src={database.slide3.src} className="d-block w-100" alt="Canyon at Nigh"/>
-                       <div className="carousel-caption d-none d-md-block">
-                           <h5>{database.slide3.title}</h5>
-                           <p>{database.slide3.description}</p>
-                       </div>  
-                   </div>
+                        <img src={database.slide1.src} className="d-block w-100" alt="Canyon at Nigh"/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>{database.slide1.title}</h5>
+                            <p>{database.slide1.description}</p>
+                        </div>  
+                    </div>
+                    <div className="carousel-item" id={database.slide2.id} style={{opacity: "100", display: "block"}}>
+                        <img src={database.slide2.src} className="d-block w-100" alt="Canyon at Nigh"/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>{database.slide2.title}</h5>
+                            <p>{database.slide2.description}</p>
+                        </div>  
+                    </div>
+                    <div className="carousel-item" id={database.slide3.id} style={{opacity: "100", display: "block"}}>
+                        <img src={database.slide3.src} className="d-block w-100" alt="Canyon at Nigh"/>
+                        <div className="carousel-caption d-none d-md-block">
+                            <h5>{database.slide3.title}</h5>
+                            <p>{database.slide3.description}</p>
+                        </div>  
+                    </div>
                 </div>
+                }
                 <button className="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span className="visually-hidden">Previous</span>
@@ -72,7 +73,7 @@ function Header() {
                 </button>
             </div>
         </div>
-    );
+    ); 
 }
 
 export default Header;
