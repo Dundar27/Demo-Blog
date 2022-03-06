0757451503c9e21c5074fccffcd624c2c3d0188b
Dundar27 <davutburakduundar@gmail.com>
2022-03-06 12:11:25 +0300

    commit text

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import { collection, onSnapshot } from "firebase/firestore";
-//import ReactDOM from 'react-dom';
-//import firebase from 'firebase/compat';
 import db from './firebase';
-
-
 
 function Header() { 
 
@@ -15,12 +11,68 @@ function Header() {
             id:doc.id,data:doc.data()
         }))))
     },[])
-
-    return(
-        <div>
-            <img src={values.map(item=>(item.data.img_src))} alt="not found img_url" />
+        
+    console.log(values[0])
+    const database = {
+        slide1: {
+            id: values[0].data.id,
+            src: values[0].data.img_src,
+            title: values[0].data.title,
+            description: values[0].data.description
+        },
+        slide2: {
+            id: values[1].data.id,
+            src: values[1].data.img_src,
+            title: values[1].data.title,
+            description: values[1].data.description
+        },
+        slide3: {
+            id: values[2].data.id,
+            src: values[2].data.img_src,
+            title: values[2].data.title,
+            description: values[2].data.description
+        }
+        
+    }
+    
+    return (
+        
+        <div className='p-5'>
+            <div id="carouselBasicExample" className="carousel slide carousel-fade" data-mdb-ride="carousel">
+                <div className="carousel-inner"> 
+                    <div className="carousel-item" id={database.slide1.id} style={{opacity: "100", display: "block"}}>
+                       <img src={database.slide1.src} className="d-block w-100" alt="Canyon at Nigh"/>
+                       <div className="carousel-caption d-none d-md-block">
+                           <h5>{database.slide1.title}</h5>
+                           <p>{database.slide1.description}</p>
+                       </div>  
+                   </div>
+                   <div className="carousel-item" id={database.slide2.id} style={{opacity: "100", display: "block"}}>
+                       <img src={database.slide2.src} className="d-block w-100" alt="Canyon at Nigh"/>
+                       <div className="carousel-caption d-none d-md-block">
+                           <h5>{database.slide2.title}</h5>
+                           <p>{database.slide2.description}</p>
+                       </div>  
+                   </div>
+                   <div className="carousel-item" id={database.slide3.id} style={{opacity: "100", display: "block"}}>
+                       <img src={database.slide3.src} className="d-block w-100" alt="Canyon at Nigh"/>
+                       <div className="carousel-caption d-none d-md-block">
+                           <h5>{database.slide3.title}</h5>
+                           <p>{database.slide3.description}</p>
+                       </div>  
+                   </div>
+                </div>
+                <button className="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Header;
