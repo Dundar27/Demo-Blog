a09ffcb48502ca1a769f900c5b0ce00aaf93385b
Dundar27 <davutburakduundar@gmail.com>
2022-03-12 13:38:47 +0300

    commit text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,38 +6,66 @@ function Header() {
 
     const [values, setvalues] = useState([]);
 
-     useEffect(()=>{
+    useEffect(()=>{
         onSnapshot(collection(db, "post"), snapshop => setvalues(snapshop.docs.map(doc => ({
             id:doc.id,data:doc.data()
         }))))
     },[])      
 
-    console.log(values[0].data.title)
+    console.log(values[1])
+
+    /* const slides = {
+        slide1 :{
+            id: values[0],
+            img_url: values[0],
+            title: values[0],
+            description: values[0]
+        },
+        slide2 :{
+            id: values[1],
+            img_url: values[1],
+            title: values[1],
+            description: values[1]
+        },
+        slide3 :{
+            id: values[2],
+            img_url: values[2],
+            title: values[2],
+            description: values[2]
+        }
+    } */
+
     return (  
         <div className='p-5'>
             <div id="carouselBasicExample" className="carousel slide carousel-fade" data-mdb-ride="carousel"> 
                 <div className="carousel-inner"> 
-                    <div className="carousel-item" id={values[0].id} style={{opacity: "100", display: "block"}}>
-                        <img src={values[0].data.img_src} className="d-block w-100" alt="Canyon at Nigh"/>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>{values[0].data.title}</h5>
-                            <p>{values[0].data.description}</p>
-                        </div>  
-                    </div>
-                    <div className="carousel-item" id={values[1].id} style={{opacity: "100", display: "block"}}>
-                        <img src={values[1].data.img_src} className="d-block w-100" alt="Canyon at Nigh"/>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>{values[1].data.title}</h5>
-                            <p>{values[1].data.description}</p>
-                        </div>  
-                    </div>
-                    <div className="carousel-item" id={values[2].id} style={{opacity: "100", display: "block"}}>
-                        <img src={values[2].data.img_src} className="d-block w-100" alt="Canyon at Nigh"/>
-                        <div className="carousel-caption d-none d-md-block">
-                            <h5>{values[2].data.title}</h5>
-                            <p>{values[2].data.description}</p>
-                        </div>  
-                    </div>
+                    {values.map(blog=>{
+                        return(
+                        <div>        
+                            <div className="carousel-item" key={values[2].id} style={{opacity: "100", display: "block"}}>
+                                <img src={values[2].data.img_src} className="d-block w-100" alt="Canyon at Nigh"/>
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>{values[2].data.title}</h5>
+                                    <p>{values[2].data.description}</p>
+                                </div>  
+                            </div>
+                            <div className="carousel-item" key={values[1].id} style={{opacity: "100", display: "block"}}>
+                                <img src={values[1].data.img_src} className="d-block w-100" alt="Canyon at Nigh"/>
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>{values[1].data.title}</h5>
+                                    <p>{values[1].data.description}</p>
+                                </div>  
+                            </div>
+                            <div className="carousel-item" key={values[0].id} style={{opacity: "100", display: "block"}}>
+                                <img src={values[0].data.img_src} className="d-block w-100" alt="Canyon at Nigh"/>
+                                <div className="carousel-caption d-none d-md-block">
+                                    <h5>{values[0].data.title}</h5>
+                                    <p>{values[0].data.description}</p>
+                                </div>  
+                            </div>
+                        </div>    
+                        )
+                    })}   
                 </div>
                 <button className="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
