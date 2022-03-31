fa3dabd11e645f80f62faf3ec6d050c07bfe0da0
Davut Burak Dündar <davutburakduundar@gmail.com>
2022-03-31 20:44:39 +0000

    commit text

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -1,6 +1,6 @@
 import { getFirestore } from '@firebase/firestore';
 import { initializeApp } from "firebase/app";
-import { Auth,getAuth } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig ={
   apiKey: "AIzaSyBVMdw0oDL8XJREpHZp6TITmrlRMikLJ_c",
@@ -13,7 +13,7 @@ const firebaseConfig ={
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = Auth(app);
+const auth = getAuth(app);
 
 export default db;
 export {auth};
