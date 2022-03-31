06076810e72eede4b1dcae73ca38b8a9d4d5dd53
Davut Burak Dündar <davutburakduundar@gmail.com>
2022-03-31 20:54:23 +0000

    commit text

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Form, Button } from 'react-bootstrap';
-import db from './Firebase';
+import db, { auth } from './Firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
 import {collection, addDoc, query} from "firebase/firestore";
 
-const Register = () => {
+class Register extends React.Component {
 
+    constructor(props){
+        this.register = this.register.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state={
+            email:"",
+            password:""
+        }
+    }
+
+    register(e){
+        e.preventDefault();
+        auth.createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
+            console.log(u);
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+
+    handleChange(e){
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
     /* //Function to run when the form is submitted
     const handleSubmit = async(event) => {
         
@@ -71,115 +95,123 @@ const Register = () => {
         }
     } */
   
-    return(
-    <div className='p-3 mt-5  container' id='register-component'>
-        <div>
-            <h1 className='text-center mb-3'>Register Form</h1>
-            <Form /* onSubmit={handleSubmit} */ className="mx-auto"> 
-                <div className='d-flex'>   
-                    <Form.Group className="mb-3 w-100" id="formBasicText">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            placeholder="John" 
-                            id="register_firstname"
-                            minLength={3}
-                            maxLength={22} 
-                            pattern={'[a-zA-Z]*'}
-                            required
-                        />
-                    </Form.Group>
-                    
-                    <Form.Group className="mb-3 w-100" id="formBasicText2">
-                        <Form.Label>Surname</Form.Label>
-                        <Form.Control 
-                            type="text" 
-                            placeholder="Doe" 
-                            id="register_surname"
-                            minLength={3}
-                            maxLength={14}
-                            pattern={'[a-zA-Z]*'}
-                            required
-                        />
-                    </Form.Group> 
+    render(){
+        return(
+            <div className='p-3 mt-5  container' id='register-component'>
+                <div>
+                    <h1 className='text-center mb-3'>Register Form</h1>
+                    <Form onSubmit={this.register} className="mx-auto"> 
+                        <div className='d-flex'>   
+                            <Form.Group className="mb-3 w-100" id="formBasicText">
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control 
+                                    type="text" 
+                                    placeholder="John" 
+                                    id="register_firstname"
+                                    minLength={3}
+                                    maxLength={22} 
+                                    pattern={'[a-zA-Z]*'}
+                                    required
+                                />
+                            </Form.Group>
+                            
+                            <Form.Group className="mb-3 w-100" id="formBasicText2">
+                                <Form.Label>Surname</Form.Label>
+                                <Form.Control 
+                                    type="text" 
+                                    placeholder="Doe" 
+                                    id="register_surname"
+                                    minLength={3}
+                                    maxLength={14}
+                                    pattern={'[a-zA-Z]*'}
+                                    required
+                                />
+                            </Form.Group> 
+                        </div>
+                                      
+                        <Form.Group className="mb-3" id="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control 
+                                type="email" 
+                                placeholder="Enter email" 
+                                name='email'
+                                id="register_email" 
+                                onChange={this.handleChange}
+                                value={this.state.email}
+                                pattern={'[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$'}
+                                required
+                            />
+                        </Form.Group>
+        
+                        <div className='d-flex'>     
+                            <Form.Group className="mb-3 w-100" id="formBasicPassword1">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control 
+                                type="password" 
+                                placeholder="Password" 
+                                name='password'
+                                id="register_password"  
+                                onChange={this.handleChange}
+                                value={this.state.password}
+                                minLength={8}
+                                maxLength={16}
+                                pattern={'(?=.*[a-z])(?=.*[A-Z]).{8,20}'}
+                                required/>
+                                <span className='form-text'>*Must contain at least one uppercase letter, lowercase letter, and number.</span>
+                            </Form.Group>
+                           
+                            <Form.Group className="mb-3 w-100" id="formBasicPassword2">
+                                <Form.Label>Repeat Password</Form.Label>
+                                <Form.Control 
+                                type="password" 
+                                placeholder="Password" 
+                                id='register_password2'
+                                minLength={8}
+                                maxLength={16}
+                                pattern={'(?=.*[A-Za-z])(?=.*[0-9]){8,20}'}
+                                required/>
+                            </Form.Group>
+                        </div>
+        
+                        <Form.Group className="mb-3" id="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me outI have read the security and privacy terms" required/>
+                        </Form.Group>
+        
+                        <div className='d-flex'>
+                            <Button className='w-100' variant="outline-success" type="submit">
+                                Submit
+                            </Button>
+                            <Link to={"/login/"} className="w-100 btn btn-outline-primary">Login</Link>
+                        </div>
+                    </Form>
                 </div>
-                              
-                <Form.Group className="mb-3" id="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                        type="email" 
-                        placeholder="Enter email" 
-                        id="register_email" 
-                        pattern={'[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$'}
-                        required
-                    />
-                </Form.Group>
-
-                <div className='d-flex'>     
-                    <Form.Group className="mb-3 w-100" id="formBasicPassword1">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control 
-                        type="password" 
-                        placeholder="Password" 
-                        id="register_password"  
-                        minLength={8}
-                        maxLength={16}
-                        pattern={'(?=.*[a-z])(?=.*[A-Z]).{8,20}'}
-                        required/>
-                        <span className='form-text'>*Must contain at least one uppercase letter, lowercase letter, and number.</span>
-                    </Form.Group>
-                   
-                    <Form.Group className="mb-3 w-100" id="formBasicPassword2">
-                        <Form.Label>Repeat Password</Form.Label>
-                        <Form.Control 
-                        type="password" 
-                        placeholder="Password" 
-                        id='register_password2'
-                        minLength={8}
-                        maxLength={16}
-                        pattern={'(?=.*[A-Za-z])(?=.*[0-9]){8,20}'}
-                        required/>
-                    </Form.Group>
+                <div className='mt-3' id='registration_successful' style={{display: "none"}}>
+                    <div className="alert alert-success" role="alert">
+                        <h4 className="alert-heading">Registration Successful!</h4>
+                        <p>You have successfully registered. You are directed to the login form to enter the system.</p>
+                    </div>
                 </div>
-
-                <Form.Group className="mb-3" id="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me outI have read the security and privacy terms" required/>
-                </Form.Group>
-
-                <div className='d-flex'>
-                    <Button className='w-100' variant="outline-success" type="submit">
-                        Submit
-                    </Button>
-                    <Link to={"/login/"} className="w-100 btn btn-outline-primary">Login</Link>
+                <div className='mt-3' id='registration_failed' style={{display: "none"}}>
+                    <div className="alert alert-danger" role="alert">
+                        <h4 className="alert-heading">Registration Failed!</h4>
+                        <p>Make sure you enter the correct values ​​and meet the requirements.</p>
+                    </div>
                 </div>
-            </Form>
-        </div>
-        <div className='mt-3' id='registration_successful' style={{display: "none"}}>
-            <div className="alert alert-success" role="alert">
-                <h4 className="alert-heading">Registration Successful!</h4>
-                <p>You have successfully registered. You are directed to the login form to enter the system.</p>
-            </div>
-        </div>
-        <div className='mt-3' id='registration_failed' style={{display: "none"}}>
-            <div className="alert alert-danger" role="alert">
-                <h4 className="alert-heading">Registration Failed!</h4>
-                <p>Make sure you enter the correct values ​​and meet the requirements.</p>
-            </div>
-        </div>
-        <div className='mt-3' id='errorPassword' style={{display: "none"}}>
-            <div className="alert alert-danger" role="alert">
-                <h4 className="alert-heading">Passwords Do Not Match!</h4>
-                <p>Please make sure you enter the passwords correctly.</p>
-            </div>
-        </div>
-        <div className='mt-3'>
-            <div className="alert alert-warning" role="alert">
-                <h4 className="alert-heading">Warning!</h4>
-                <p>Our site is still under development. You are now using the demo version. If you've discovered a bug, please let us know.</p>
-            </div>
-        </div>
-    </div>    
-    )
+                <div className='mt-3' id='errorPassword' style={{display: "none"}}>
+                    <div className="alert alert-danger" role="alert">
+                        <h4 className="alert-heading">Passwords Do Not Match!</h4>
+                        <p>Please make sure you enter the passwords correctly.</p>
+                    </div>
+                </div>
+                <div className='mt-3'>
+                    <div className="alert alert-warning" role="alert">
+                        <h4 className="alert-heading">Warning!</h4>
+                        <p>Our site is still under development. You are now using the demo version. If you've discovered a bug, please let us know.</p>
+                    </div>
+                </div>
+            </div>    
+        )
+    }
 }
 
 export default Register;
