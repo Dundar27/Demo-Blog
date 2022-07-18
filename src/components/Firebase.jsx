0b93ad238f6adbd0a318dd6dd1badb318909cecf
Davut Burak Dündar <86006681+Dundar27@users.noreply.github.com>
2022-07-18 10:07:42 +0300

    Update Firebase.jsx

--- src/components/Firebase.jsx
+++ src/components/Firebase.jsx
@@ -1,20 +1,21 @@
-import { getFirestore } from '@firebase/firestore';
+import { getFirestore } from "@firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
 
-const firebaseConfig ={
+const firebaseConfig = {
   apiKey: "AIzaSyBVMdw0oDL8XJREpHZp6TITmrlRMikLJ_c",
   authDomain: "fir-school-page.firebaseapp.com",
   projectId: "fir-school-page",
   storageBucket: "fir-school-page.appspot.com",
   messagingSenderId: "59211089125",
-  appId: "1:59211089125:web:94bd095b04af729789c00c"
+  appId: "1:59211089125:web:94bd095b04af729789c00c",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const storage = getStorage();
 
 export default db;
-export {auth};
-
+export { auth, storage };
