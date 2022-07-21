6870726d5798513ce66e2545c252f5a4ea3a8543
Dundar27 <davutburakduundar@gmail.com>
2022-07-21 22:51:04 +0300

    2 files modified

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -240,7 +240,7 @@ class App extends React.Component {
           ></Route>
 
           <Route
-            path="/profile/:username"
+            path="/profile/"
             element={
               this.state.user ? (
                 <div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -8,73 +8,97 @@ import BlogPosts from './BlogPosts';
 import ProfileCard from './ProfileCard';
 
 class Profile extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state= {
+    this.state = {
       userData: [],
       userBlogPosts: [],
-      searchQuery:""
-    }
+      searchQuery: '',
+    };
   }
 
-  username  = useParams();
-
-  async getUserData() {
-    const response = await onSnapshot(query(collection(db, 'users'), where("username", "==", this.username)), snapshop => this.setState({userData: snapshop.docs.map(doc => ({
-      id:doc.id,data:doc.data()
-    }))}));
-  }
-
-  async getUserBlogPosts() {
-    const response = await onSnapshot(query(collection(db, 'blogs'), where("writer", "==", this.username)), snapshop => this.setState({userBlogPosts: snapshop.docs.map(doc => ({
-      id:doc.id,data:doc.data()
-    }))}));
-  }
-
-  componentDidMount(){
+  componentDidMount() {
     this.getUserData();
     this.getUserBlogPosts();
   }
 
+  //params  = useParams();
+
+  async getUserData() {
+    const response = await onSnapshot(
+      query(
+        collection(db, 'users'),
+        where('username', '==', this.props.userControl.displayName)
+      ),
+      (snapshop) =>
+        this.setState({
+          userData: snapshop.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          })),
+        })
+    );
+  }
+
+  async getUserBlogPosts() {
+    const response = await onSnapshot(
+      query(
+        collection(db, 'blogs'),
+        where('writer', '==', this.props.userControl.displayName)
+      ),
+      (snapshop) =>
+        this.setState({
+          userBlogPosts: snapshop.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          })),
+        })
+    );
+  }
+
   searchBlogPostProp = (event) => {
     this.setState({ searchQuery: event.target.value });
-  }
-  
-  render(){
+  };
 
-  let filtered = this.state.userBlogPosts
-    .filter((blog) => {
-      return (
-        blog.data.title.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
-      );
-    })
-    .sort((a, b) => {
-      return a.id < b.id ? 1 : a.id > b.id ? -1 : 0;
-    });
+  render() {
+    let filtered = this.state.userBlogPosts
+      .filter((blog) => {
+        return (
+          blog.data.title
+            .toLowerCase()
+            .indexOf(this.state.searchQuery.toLowerCase()) !== -1
+        );
+      })
+      .sort((a, b) => {
+        return a.id < b.id ? 1 : a.id > b.id ? -1 : 0;
+      });
 
-  return (
-    <div id="account-component">
-      <div className="p-5">
-        <Row>
-          <Col sm={3}>
-            <ProfileCard userData={this.state.userData} />
-          </Col>
-          <Col sm={9}>
-            <div>
-              <h4>Abouth Me</h4> <br />
-              <p>{this.state.userData.map((user) => user.data.usermessage)}</p> <br />
-            </div>
-            <div>
-              <h4>My Blog Posts</h4> <hr />
-              <SearchBar searchProp={this.searchBlogPostProp} />
-              <BlogPosts BlogPosts={filtered} />
-            </div>
-          </Col>
-        </Row>
+    return (
+      <div id="account-component">
+        <div className="p-5">
+          <Row>
+            <Col sm={3}>
+              <ProfileCard userData={this.state.userData} />
+            </Col>
+            <Col sm={9}>
+              <div>
+                <h4>Abouth Me</h4> <br />
+                <p>
+                  {this.state.userData.map((user) => user.data.usermessage)}
+                </p>{' '}
+                <br />
+              </div>
+              <div>
+                <h4>My Blog Posts</h4> <hr />
+                <SearchBar searchProp={this.searchBlogPostProp} />
+                <BlogPosts BlogPosts={filtered} />
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
-    </div>
-  );
+    );
   }
-};
+}
 
 export default Profile;
